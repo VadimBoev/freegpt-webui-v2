ddcd06a0de129d6b7a9ad7596e302b758689596f
Vadim Boev <33985574+VadimBoev@users.noreply.github.com>
2023-09-29 12:25:43 +0300

    fix WindowsProactorEventLoopPolicy for Windows
    WindowsProactorEventLoopPolicy is not supported, you can use the selector loop by:
    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())

--- server/request.py
+++ server/request.py
@@ -1,13 +1,15 @@
 import g4f
 import sys
+import platform
 
 #this is for the future
 #import json
 
-#import asyncio
+import asyncio
 
 #its fix for Windows
-#asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+if platform.system() == "Windows":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 response = g4f.ChatCompletion.create(
     model="gpt-3.5-turbo",
